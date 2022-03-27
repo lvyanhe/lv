14d13608aaf9e484ce4e17945d17c23038490337
lvyanhe <101860990+lvyanhe@users.noreply.github.com>
2022-03-27 16:57:30 +0800

    Add files via upload

---
+++ 和.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+    int n;
+    int maxSum=0;
+    int nowSum=0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        int num;
+        cin>>num;
+        nowSum+=num;
+        if(nowSum>maxSum)
+			maxSum=nowSum;
+        if(nowSum<0)
+			nowSum=0;
+
+    }
+    cout<<maxSum<<endl;
+	system("pause");
+    return 0;
+}
